586d9e8e410627cb352728887fbff933ac020d68
Your Name <mr.shikhar100@gmail.com>
2026-04-14 01:10:00 +0530

    Changes

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -2,7 +2,10 @@ const nodemailer = require("nodemailer");
 
 const createTransporter = async () => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false,
+    family:4,
     auth: {
       type: "OAuth2",
       user: process.env.GOOGLE_USER,
