eef14801558a62e2bc781428f70d67ee55b2f54e
Your Name <mr.shikhar100@gmail.com>
2026-04-14 12:13:08 +0530

    Changes

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -36,8 +36,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save",async function (next) {
-    if(!this.isModified("password") || !this.password) return
+    if(!this.isModified("password") || !this.password) return next();
     this.password = await bcrypt.hash(this.password,12);
+    next();
 });
 
 
